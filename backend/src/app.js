b1824ae8fa48da2a9d30595f6447e076d17a0f09
Dracarys <ruchikumari281105@gmail.com>
2025-06-12 18:08:53 +0530

    changed controllers

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,7 @@ app.use(cors({
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended: true, limit: "50mb"}))
 app.use(cookieParser())
+app.use(express.static("public"));
 
 const server = http.createServer(app);
 const io = new Server(server, {
